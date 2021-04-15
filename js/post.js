a8f58df6ca822f4fd9bd28de19ac8a417bd5d986
junhok82 <junhok82@gmail.com>
2021-04-15 10:23:45 +0900

    마크다운

--- js/post.js
+++ js/post.js
@@ -1,74 +1,64 @@
-$(document).ready(function() {
-
-    class App {
-        constructor(data) {
-          this.init(data);
-          this.addEventListeners();
-        }
-      
-        init(data) {
-            
-            let textArea = document.createElement("textarea");
-            textArea.value = data;
-    
-            let origin = $("#origin");
-            origin.append(textArea);
-            origin.css('display', 'none');
-            
-    
-          const input = this.input = CodeMirror.fromTextArea(
-            textArea,
-          {
-            lineNumbers: true,
-            matchBrackets: true,
-            mode: "text/x-markdown",
-            theme: "material" });
-      
-      
-          const output = this.output = CodeMirror.fromTextArea(
-          document.querySelector(".js-output"),
-          {
-            lineNumbers: true,
-            matchBrackets: true,
-            mode: "text/html",
-            theme: "material",
-            readOnly: true });
-      
-      
-      
-          this.preview = document.querySelector(".js-preview");
-      
-          output.getWrapperElement().classList.add("CodeMirror-readonly");
-      
-          this.compile(input.getValue());
-        }
-      
-        compile(source) {
-          const previewDocument = this.preview.contentDocument;
-          const output = marked(source);
-      
-          previewDocument.open();
-          previewDocument.write(output);
-          $('.js-preview').css('height', previewDocument.body.scrollHeight);
-          previewDocument.close();
-      
-          this.output.setValue(output);
-        }
-      
-        addEventListeners() {
-          const { input } = this;
-          let delay;
-      
-          input.on("change", () => {
-            clearTimeout(delay);
-            delay = setTimeout(() => this.compile(input.getValue()), 300);
-          });
-        }
-    }
-
-    const data = '# Angular Markdown Editor\n\n> GitHub Flavored Markdown Editor\n\n# Built with\n- **[marked](https://github.com/chjj/marked)** for Parsing Markdown\n- **[angular-marked](https://github.com/Hypercubed/angular-marked)** for easier usage of *marked*\n- **[highlight-js](https://github.com/isagalaev/highlight.js)** for code highlighting\n- **[github-markdown-css](https://github.com/sindresorhus/github-markdown-css)** for beautifying markdown output\n- **[highlight-js css](https://github.com/isagalaev/highlight.js/tree/master/src/styles)** for beautifying code output\n\n# Examples\n* Javascript\n\n```javascript\nfunction() {	\n  console.log("This is awesome!");\n}\n```\n\n* Bash\n\n```bash\n# step 1\nnpm install\n```\netc..\n\n# The End\n- Enjoy ~\n # Angular Markdown Editor\n\n> GitHub Flavored Markdown Editor\n\n# Built with\n- **[marked](https://github.com/chjj/marked)** for Parsing Markdown\n- **[angular-marked](https://github.com/Hypercubed/angular-marked)** for easier usage of *marked*\n- **[highlight-js](https://github.com/isagalaev/highlight.js)** for code highlighting\n- **[github-markdown-css](https://github.com/sindresorhus/github-markdown-css)** for beautifying markdown output\n- **[highlight-js css](https://github.com/isagalaev/highlight.js/tree/master/src/styles)** for beautifying code output\n\n# Examples\n* Javascript\n\n```javascript\nfunction() {	\n  console.log("This is awesome!");\n}\n```\n\n* Bash\n\n```bash\n# step 1\nnpm install\n```\netc..\n\n# The End\n- Enjoy ~\n'
-    new App(data);
-    // 
+ // Directory for emoji images
+ emojify.setConfig({
+    img_dir          : 'https://raw.githubusercontent.com/hassankhan/emojify.js/c9568789ee1602b3beccca9f4e43b5c4b2b9a882/images/emoji' 
 });
 
+////////////////////////////////////////
+$(document).ready(function() {
+});
 
+var app = angular.module('cm', []);
+
+
+app.controller('ctrl', function($scope) {
+    $scope.my_markdown = "# Codemirror Markdown Editor\n\n> Built with Angular JS Custom Directive !\n\n# Feature\n- GitHub Flavored Preview\n- Emoji Support !! :smile: :+1:\n\n# Built with\n\n- **[marked](https://github.com/chjj/marked)** for Parsing Markdown\n- **[emoji](http://hassankhan.me/emojify.js/)** for emoji's ! :kissing_heart: :scream: :speech_balloon:\n- **[highlight-js](https://github.com/isagalaev/highlight.js)** for code highlighting\n- **[github-markdown-css](https://github.com/sindresorhus/github-markdown-css)** for beautifying markdown output\n- **[highlight-js css](https://github.com/isagalaev/highlight.js/tree/master/src/styles)** for beautifying code output\n\n\n# Examples\n* Javascript\n\n```javascript\nfunction() {	\n  console.log('This is awesome!');\n}\n```\n\n* Bash\n\n```bash\n# step 1\nnpm install\n```\netc..\n\n# The End\n- Enjoy ~";
+});
+
+app.directive('codeEditor', function($timeout) {
+    return {
+        restrict: 'E',
+        require: "?ngModel",
+        replace: true,
+        transclude: true,
+        theme: 'base16-light',
+        template: '<div class="code-editor"></div>',
+        link: function(scope, element, attrs, ngModelCtrl, transclude) {
+            scope.editor = CodeMirror(element[0], {
+                mode: 'gfm',
+                autoCloseBrackets: true,
+                lineWrapping: true,
+                lineNumbers: true,
+                matchBrackets: true,
+                theme: 'base16-light',
+                extraKeys: {
+                    "Enter": "newlineAndIndentContinueMarkdownList"
+                }
+            });
+            if (ngModelCtrl) {
+                ngModelCtrl.$render = function() {
+                    scope.editor.setValue(ngModelCtrl.$viewValue);
+                }
+            }
+            scope.editor.on('change', function() {
+                ngModelCtrl.$setViewValue(scope.editor.getValue());
+                scope.$emit('editor-change')
+            })
+        }
+    }
+})
+
+app.directive("markdownViewer", [function() {
+    marked.setOptions({
+        highlight: function(code) {
+            return hljs.highlightAuto(code).value;
+        }
+    })
+    return {
+        link: function(scope, element, attrs) {
+            scope.$on('editor-change', function() {
+                element.html(marked(scope.editor.getValue()));
+                emojify.run(document.getElementById('emo'));
+            })
+        }
+    }
+}]);
